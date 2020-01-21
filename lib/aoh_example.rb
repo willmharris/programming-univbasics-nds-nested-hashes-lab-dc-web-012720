0bb279b9b930afdb0a377584115ea08bbe268b36
William Harris <willmharris0@gmail.com>
2020-01-21 18:13:25 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,7 @@ end
 # Using Array literal syntax only, build a nested array that uses the data
   # held in the constants
 def literal_aoh
-  
+  literal_array = [{ name:  "Don Gately", occupation: "Live-in Staff" }, { name:  "Joelle van Dyne", occupation: "Radio Personality" }, { name:  "Pat Monteseian", occupation: "Staff" }, { name:  "Kate Gompert", occupation: "None" }, { name:  "Bruce Green", occupation: "Fan of Mildred" } ]
 end
 
 def aoh_lookup(aoh, row, key)
