a47350a780d4a08c5e6bb607b1333c87219467f6
William Harris <willmharris0@gmail.com>
2020-01-21 18:08:50 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -9,7 +9,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
+  # Using Array literal syntax only, build a nested array that uses the data
   # held in the constants
 end
 
